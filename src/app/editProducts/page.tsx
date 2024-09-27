f7ca7aee9bbd24c6345d2ee49b4764ad3198b868
Sol Calderon <calderonsol81@gmail.com>
2024-09-27 12:55:59 -0600

    lk

--- src/app/editProducts/page.tsx
+++ src/app/editProducts/page.tsx
@@ -33,11 +33,13 @@ const EditProducts = () => {
                   Ingresa los datos nuevos del productos
                 </p>
 
-                <input
-                  type="file"
-                  name="Camboiar"
-                  className="border-stroke mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:hover:border-primary dark:hover:bg-primary/5 dark:hover:text-primary dark:hover:shadow-none"
-                />
+                <div className="flex flex-row items-center justify-evenly ">
+                  <input
+                    type="file"
+                    name="Camboiar"
+                    className="border-stroke mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:hover:border-primary dark:hover:bg-primary/5 dark:hover:text-primary dark:hover:shadow-none"
+                  />
+                </div>
 
                 <form className="flex flex-col gap-3">
                   <div className="rounded-3xl p-2 ">
