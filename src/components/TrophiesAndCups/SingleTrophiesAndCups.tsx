9d98703dc78b165314f185548aaaea4c397fdc3e
Sol Calderon <calderonsol81@gmail.com>
2025-03-17 16:47:53 -0600

    4:47

--- src/components/TrophiesAndCups/SingleTrophiesAndCups.tsx
+++ src/components/TrophiesAndCups/SingleTrophiesAndCups.tsx
@@ -84,7 +84,7 @@ const SingleTrophiesAndCups = () => {
                   style={{ backgroundColor }}
                 >
                   <span className="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full  bg-blue-950 px-4 py-2 text-sm font-semibold capitalize text-white">
-                    {trophy.category} | "Trofeos"
+                    {trophy.category}
                   </span>
                   <img
                     className="mx-auto object-contain"
